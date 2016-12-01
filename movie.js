38b0416269faf15014dbf3be4c92ab6cf9118ebf
ScottPartacz <Sparatcz@hawk.iit.edu>
2016-11-30 22:16:03 -0600

    added the api pull request $.get(url)

--- movie.js
+++ movie.js
@@ -6,7 +6,10 @@ $(document).ready(function(){
         e.preventDefault(); 
         var poster = $('#movie').val();
         var url = 'http://www.omdbapi.com/?t=' + poster + '&r=json' + '&plot=full';
-        
+        $.get(url).done(
+          
+          
+    });
 });
    
 })(jQuery);
