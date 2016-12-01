551b608e1342d6fe2f4f32a629f9cbea7f45dc01
ScottPartacz <Sparatcz@hawk.iit.edu>
2016-11-30 22:16:56 -0600

    added the data we pulled from the api

--- movie.js
+++ movie.js
@@ -7,7 +7,20 @@ $(document).ready(function(){
         var poster = $('#movie').val();
         var url = 'http://www.omdbapi.com/?t=' + poster + '&r=json' + '&plot=full';
         $.get(url).done(
-          
+          function(data,json) {
+            var movie = data.Poster;
+           
+              
+              $('#Rating').text('Rated: ' + data.Rated);
+              $('#Release').text('Release: ' + data.Released);
+              $('#Runtime').text('Runtime: ' + data.Runtime);
+              $('#poster').html('<img id="thePoster" src=' + movie + ' alt = "Poster Not Found" />');
+              $('#Plot').text('Plot: ' + data.Plot);
+              $('#Metascore').text('MetaScore: ' + data.Metascore);
+              $('#imdb').text('IMDbRating: ' + data.imdbRating);
+            
+            
+          })
           
     });
 });
