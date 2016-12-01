09e334ad23b4fc6d6b983a23847799cf83681ec8
ScottPartacz <Sparatcz@hawk.iit.edu>
2016-11-30 22:17:28 -0600

    Added an error checker

--- movie.js
+++ movie.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
         $.get(url).done(
           function(data,json) {
             var movie = data.Poster;
-           
-              
+            if(movie != null) {
+              $('#ERROR').empty();
               $('#Rating').text('Rated: ' + data.Rated);
               $('#Release').text('Release: ' + data.Released);
               $('#Runtime').text('Runtime: ' + data.Runtime);
@@ -18,8 +18,17 @@ $(document).ready(function(){
               $('#Plot').text('Plot: ' + data.Plot);
               $('#Metascore').text('MetaScore: ' + data.Metascore);
               $('#imdb').text('IMDbRating: ' + data.imdbRating);
-            
-            
+            }
+            else {
+              $('#ERROR').text('Error: ' + data.Error);
+              $('#Rating').empty();
+              $('#Release').empty();
+              $('#Runtime').empty();
+              $('#Plot').empty();
+              $('#Metascore').empty();
+              $('#imdb').empty();
+              $('#poster').html('<img id="thePoster" src="http://vignette2.wikia.nocookie.net/assassinscreed/images/3/39/Not-found.jpg/revision/latest?cb=20110517171552" alt = "No Photo found" />');
+            }
           })
           
     });
