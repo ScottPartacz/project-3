fa143566ccf758d07f1093c40f438d5a2265e05a
ScottPartacz <Sparatcz@hawk.iit.edu>
2016-11-30 22:15:15 -0600

    getting the movie to api from user submited data

---
+++ movie.js
@@ -0,0 +1,12 @@
+$.noConflict();
+(function($) {
+  
+$(document).ready(function(){
+    $('#movie-form').on('submit', function(e) { 
+        e.preventDefault(); 
+        var poster = $('#movie').val();
+        var url = 'http://www.omdbapi.com/?t=' + poster + '&r=json' + '&plot=full';
+        
+});
+   
+})(jQuery);
